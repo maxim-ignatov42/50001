4d8c5680f11b47cdff35b4221c489ec5ea8d552c
Ignatov Maxim <ignatov.mv@edu.spbstu.ru>
2026-05-26 12:43:24 +0300

    Fixed intendation error

--- ignatov.maxim/T3/iofmtguard.h
+++ ignatov.maxim/T3/iofmtguard.h
@@ -5,14 +5,13 @@
 class iofmtguard
 {
 public:
-	iofmtguard(std::basic_ios< char >& s);
-	~iofmtguard();
-
+    iofmtguard(std::basic_ios< char >& s);
+    ~iofmtguard();
 private:
-	std::basic_ios< char >& s_;
-	std::streamsize width_;
-	char fill_;
-	std::streamsize precision_;
-	std::basic_ios< char >::fmtflags fmt_;
+    std::basic_ios< char >& s_;
+    std::streamsize width_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
 };
 #endif /* IOF_MT_GUARD_H */
